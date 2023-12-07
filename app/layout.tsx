58ea7f60632406c261f2ac73ac6609415ee804a8
evanf <evanfungv@gmail.com>
2023-12-07 01:35:44 -0500

    add local font

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,37 +11,37 @@ export const metadata: Metadata = {
 const clashDisplay = localFont({
   src: [
     {
-      path: 'fonts/ClashDisplay-Regular.woff',
+      path: '../public/fonts/ClashDisplay-Regular.woff',
       weight: '400',
       style: 'normal',
     },
-    // {
-    //   path: 'fonts/ClashDisplay-Bold.woff2',
-    //   weight: '700',
-    //   style: 'bold',
-    // },
-    // {
-    //   path: 'fonts/ClashDisplay-Extralight.woff2',
-    //   weight: '200',
-    //   style: 'extralight',
-    // },
-    // {
-    //   path: './fonts/ClashDisplay-Light.woff2',
-    //   weight: '300',
-    //   style: 'light',
-    // },
-    // {
-    //   path: './fonts/ClashDisplay-Medium.woff2',
-    //   weight: '500',
-    //   style: 'medium',
-    // },
-    // {
-    //   path: './fonts/ClashDisplay-Semibold.woff2',
-    //   weight: '600',
-    //   style: 'semibold',
-    // },
+    {
+      path: '../public/fonts/ClashDisplay-Bold.woff2',
+      weight: '700',
+      style: 'bold',
+    },
+    {
+      path: '../public/fonts/ClashDisplay-Extralight.woff2',
+      weight: '200',
+      style: 'extralight',
+    },
+    {
+      path: '../public/fonts/ClashDisplay-Light.woff',
+      weight: '300',
+      style: 'light',
+    },
+    {
+      path: '../public/fonts/ClashDisplay-Medium.woff',
+      weight: '500',
+      style: 'medium',
+    },
+    {
+      path: '../public/fonts/ClashDisplay-Semibold.woff',
+      weight: '600',
+      style: 'semibold',
+    },
   ],
-  variable: "--font-clash-bold",
+  variable: "--font-clash-display",
 })
 
 export default function RootLayout({
@@ -51,7 +51,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body suppressHydrationWarning={true} className={`${clashDisplay.variable}`}>{children}</body>
+      <body suppressHydrationWarning={true} className={clashDisplay.className}>{children}</body>
     </html>
   )
 }
